fb9630fedfb266156dfed04464cdfe5dcf3f003c
Youhana Hana <meyouhana@gmail.com>
2018-06-01 13:12:00 +0100

    Updated provision script for botj local VM and K8S to use environment variables

--- 00-start-here/provision.sh
+++ 00-start-here/provision.sh
@@ -3,6 +3,13 @@ export KUBECTL_VERSION="1.10.0"
 export KOPS_VERSION="1.9.1"
 export TERRAFORM_VERSION="0.11.7"
 
+function echoToProfile {
+    profile_file="$HOME/.profile"
+    if ! grep -q "$1" "${profile_file}" ; then
+        echo "$1" >> "${profile_file}"
+    fi
+}
+
 # Change to Vagrant home directory
 cd /home/vagrant
 
@@ -39,4 +46,29 @@ sudo chmod +x kops-linux-amd64
 sudo mv kops-linux-amd64 /usr/local/bin/kops
 
 # Setup autocomplete for Kubectl
-echo "source <(kubectl completion bash)" >>~/.profile
+echoToProfile "source <(kubectl completion bash)"
+# echo "source <(kubectl completion bash)" >>~/.profile
+
+# export dev ENV variables to be used by provision script
+echoToProfile "export dev_AWS_REGION=us-east-1" #US East (N. Virginia)
+echoToProfile "export dev_AWS_AZ=us-east-1a" #availability zone
+echoToProfile "export dev_CLUSTER_MASTER_SIZE=t2.micro"  #use t2 micro for the master node
+echoToProfile "export dev_CLUSTER_NODE_SIZE=t2.nano"  #use t2.nano for the worker nodes
+echoToProfile "export dev_CLUSTER_NODE_COUNT=2"  #number of worker nodes
+echoToProfile "export dev_CLUSTER_VERSION=1.10.0"  #K8s version
+echoToProfile "export dev_CLUSTER_DOMAIN_NAME=youhanalabs.com" #Domain name registered by AWS
+
+# export production ENV variables to be used by provision script
+echoToProfile "export prod_AWS_REGION=us-east-1" #US East (N. Virginia)
+echoToProfile "export prod_AWS_AZ=us-east-1a,us-east-1b,us-east-1d" #availability zone
+echoToProfile "export prod_CLUSTER_MASTER_SIZE=t2.medium" #use t2 medium for the master node
+echoToProfile "export prod_CLUSTER_NODE_SIZE=t2.small" #use t2.small for the worker nodes
+echoToProfile "export prod_CLUSTER_NODE_COUNT=6" #number of worker nodes
+echoToProfile "export prod_CLUSTER_VERSION=1.10.0" #K8s version
+echoToProfile "export prod_CLUSTER_DOMAIN_NAME=youhanalabs.com" #Domain name registered by AWS
+
+# Compile ~/.profile to be ready for the same shell
+source ~/.profile
+
+# clone repo
+git clone https://github.com/Youhana-Hana/Infra-java-prevayler-K8S.git ~/infra-java-prevayler-K8S
--- 01-deploy-k8s/provision.sh
+++ 01-deploy-k8s/provision.sh
@@ -3,43 +3,73 @@
 # Exit on first failure and treat unset variables as error
 set -eu
 
-# check AWS credentials exported
-if [ -z ${AWS_ACCESS_KEY_ID+x} ]; then
-    echo "AWS_ACCESS_KEY_ID is unset";
-    exit 1;
-fi
+# check environment variable is set
+function checkEnv {
+    if [ -z ${1+x} ]; then
+        echo "$1 is unset";
+        exit 1;
+    fi
+}
 
-if [ -z ${AWS_SECRET_ACCESS_KEY+x} ]; then
-    echo "AWS_SECRET_ACCESS_KEY is unset";
+# check AWS credentials exported
+checkEnv "AWS_ACCESS_KEY_ID"
+checkEnv "AWS_SECRET_ACCESS_KEY"
+
+# Cluster alias default to dev
+export CLUSTER_ALIAS="dev"
+
+while getopts t: option
+do
+    case "${option}"
+    in
+        t) CLUSTER_ALIAS=${OPTARG};;
+    esac
+done
+
+if [[ "${CLUSTER_ALIAS}" =~ ^(dev|prod)$ ]]; then
+    echo "Target evnironment must be dev or prod";
     exit 1;
 fi
 
 # Domain name that is hosted in AWS Route 53
-export DOMAIN_NAME="youhanalabs.com"
-
-# Cluster alias
-export CLUSTER_ALIAS="dev"
+export DOMAIN_NAME="${CLUSTER_ALIAS}_CLUSTER_DOMAIN_NAME";
 
 # Cluster Full DNS name
 export CLUSTER_FULL_NAME="${CLUSTER_ALIAS}.${DOMAIN_NAME}"
 
 # AWS availability zone where the cluster will be created
-export CLUSTER_AWS_AZ="us-east-1a"
+export CLUSTER_AWS_AZ="${CLUSTER_ALIAS}_AWS_AZ"
 
 export CLUSTER_FULL_NAME="${CLUSTER_ALIAS}.${DOMAIN_NAME}"
+
+export CLUSTER_MASTER_AWS_EC2_SIZE="${CLUSTER_ALIAS}_CLUSTER_MASTER_SIZE"
+export CLUSTER_NODE_AWS_EC2_SIZE="${CLUSTER_ALIAS}_CLUSTER_NODE_SIZE"
+export CLUSTER_NODE_COUNT="${CLUSTER_ALIAS}_CLUSTER_NODE_COUNT"
+export K8S_VERSION="${CLUSTER_ALIAS}_CLUSTER_VERSION"
 
 # Create a S3 bucket directly in AWS, which Kops will use to store all of the cluster configuration information:
 aws s3api create-bucket --bucket ${CLUSTER_FULL_NAME}-state
 
 # Export S3 URL
 export KOPS_STATE_STORE="s3://${CLUSTER_FULL_NAME}-state"
+echo "export KOPS_STATE_STORE=s3://${CLUSTER_FULL_NAME}-state" >>~/.profile #kops s3 bucket state
 
 kops create cluster \
      --name=${CLUSTER_FULL_NAME} \
+     --master-zones=${CLUSTER_AWS_AZ} \
      --zones=${CLUSTER_AWS_AZ} \
-     --master-size="t2.micro" \
-     --node-size="t2.nano" \
-     --node-count="2" \
+     --master-size=${CLUSTER_MASTER_AWS_EC2_SIZE} \
+     --node-size=${CLUSTER_NODE_AWS_EC2_SIZE} \
+     --node-count=${CLUSTER_NODE_COUNT} \
      --dns-zone=${DOMAIN_NAME} \
+     --kubernetes-version=${K8S_VERSION} \
      --ssh-public-key="~/.ssh/id_rsa.pub" \
-     --kubernetes-version="1.10.0"
+     --yes
+#Create a cluster context alias in your kubeconfig file
+kubectl config set-context ${CLUSTER_ALIAS} --cluster=${CLUSTER_FULL_NAME} --user=${CLUSTER_FULL_NAME}
+
+#Use this new cluster context
+kubectl config use-context ${CLUSTER_ALIAS}
+
+# Smoke test, get cluster nodes
+kubectl get nodes
